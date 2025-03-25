625743aac77c98de731f4894537c2486aaa26721
viethoangkxtb <viethoangkxtb@gmail.com>
2025-03-25 12:56:44 +0700

    done week 1

--- src/SUM_ARRAY.java
+++ src/SUM_ARRAY.java
@@ -4,6 +4,19 @@ import java.util.Scanner;
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class SUM_ARRAY {
     public static void main(String[] args) {
+        int n;
+        int temp;
+        int s = 0;
 
+        Scanner myObj = new Scanner(System.in);
+
+        n = myObj.nextInt();
+
+        for (int i = 0; i < n; i++) {
+            temp = myObj.nextInt();
+            s = s + temp;
+        }
+
+        System.out.println(s);
     }
 }
